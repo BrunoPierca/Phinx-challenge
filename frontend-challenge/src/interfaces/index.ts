31bcf6624581462d7af2463997c8ee2d70e1049b
BrunoPierca <pierca18@gmail.com>
2024-09-02 06:03:02 -0300

    Added types for Battles

---
+++ frontend-challenge/src/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface Pokemon {
+    id: string,
+    name: string,
+    attack: number
+    defense: number
+    hp: number
+    speed: number
+    type: string,
+    imageUrl: string,
+    [key: string]: number | string;
+}
+
+export interface Battle {
+    id: number;
+
+    winner: Pokemon;
+
+    loser: Pokemon;
+
+    date: Date;
+}
