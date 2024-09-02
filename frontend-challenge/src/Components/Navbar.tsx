df2c5e65922e9c0741826bbf6de6998dc387016a
BrunoPierca <pierca18@gmail.com>
2024-09-02 08:38:29 -0300

    Create Navbar.tsx

---
+++ frontend-challenge/src/Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Button,  Stack, Typography } from "@mui/material"
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import { useState } from "react";
+import { BattleLog } from "./BattleLog";
+
+export const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+            <Typography variant="h3" >
+                Battle of Pokemon
+            </Typography>
+            <Button variant="contained" sx={{ minWidth: 0, width: 40, height: 40 }}
+                onClick={handleOpen}
+            >
+                <LeaderboardIcon />
+            </Button>
+
+            <BattleLog
+                open={open}
+                handleClose={handleClose}
+            />
+        </Stack>
+    )
+}
