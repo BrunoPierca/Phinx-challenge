900ca825135ecd22f3f643ca708d009b9b78febb
BrunoPierca <pierca18@gmail.com>
2024-09-02 03:22:01 -0300

    Added FindAll and FindOne functionality

--- backend-challenge/src/pokemon/pokemon.service.ts
+++ backend-challenge/src/pokemon/pokemon.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pokemon } from './entities/pokemon.entity';
+import {  Repository } from 'typeorm';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
-  }
+
+  constructor(
+    @InjectRepository(Pokemon)
+    private readonly pokemonRepository: Repository<Pokemon>,
+  ) { }
+
+  private readonly logger = new Logger('Pokemon service')
 
   findAll() {
-    return `This action returns all pokemon`;
+    try {
+      return this.pokemonRepository.find()
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(id: string) {
+    let pokemon: Pokemon
+    pokemon = await this.pokemonRepository.findOneBy({ id })
+    if (!pokemon) throw new NotFoundException(`Couldn't find Pokemon with ID: ${id}`)
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  private handleDBExceptions(error: any) {
+    if (error.status === 400) {
+      throw new BadRequestException(error.message)
+    }
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.error(error)
+    throw new InternalServerErrorException(`Check logs!  error: ${error}`)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
-  }
 }
