9aa172d8882814ef23d4eb2141393a8d16a6b42e
BrunoPierca <pierca18@gmail.com>
2024-09-02 03:22:17 -0300

    Updated imports/export

--- backend-challenge/src/pokemon/pokemon.module.ts
+++ backend-challenge/src/pokemon/pokemon.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports: [TypeOrmModule.forFeature([Pokemon])],
+  exports: [PokemonService]
 })
-export class PokemonModule {}
+export class PokemonModule { }
