03fac88f7e26f2c8fc02a3280f3be85d653f8875
BrunoPierca <pierca18@gmail.com>
2024-09-02 08:39:13 -0300

    MVP

--- frontend-challenge/src/App.tsx
+++ frontend-challenge/src/App.tsx
@@ -1,62 +1,43 @@
-import { Box, Button, Card, Container, Grid2 as Grid, Stack, Typography } from "@mui/material"
-import { PokemonCard } from './Components/PokemonCard';
+import { Alert, CircularProgress, Container } from "@mui/material"
+import { PokemonGrid } from "./Components/PokemonGrid";
+import { WinnerBanner } from "./Components/WinnerBanner";
+import { useEffect, useState } from "react";
+import { getPokemon } from "./Services/API";
+import { Pokemon } from "./interfaces";
+import { BattleSection } from "./Components/BattleSection";
+import { Navbar } from './Components/Navbar';
 
-const placeholderPokemon = [
-  {
-    id: "pokemon-1",
-    name: "Pikachu",
-    attack: 4,
-    defense: 3,
-    hp: 3,
-    speed: 6,
-    type: "Type",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png"
-  },
-  {
-    id: "pokemon-2",
-    name: "Charmander",
-    attack: 4,
-    defense: 3,
-    hp: 3,
-    speed: 4,
-    type: "Type",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png"
-  },
-  {
-    id: "pokemon-3",
-    name: "Squirtle",
-    attack: 3,
-    defense: 4,
-    hp: 3,
-    speed: 3,
-    type: "Type",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png"
-  },
-  {
-    id: "pokemon-4",
-    name: "Bulbasur",
-    attack: 4,
-    defense: 3,
-    hp: 3,
-    speed: 3,
-    type: "Type",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png"
-  },
-  {
-    id: "pokemon-5",
-    name: "Eevee",
-    attack: 4,
-    defense: 3,
-    hp: 4,
-    speed: 5,
-    type: "Type",
-    imageUrl: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/133.png"
-  }
-]
+const fetchPokemon = async () => await getPokemon()
 
+const getRandomExcluding = (excludedNumber: number, max: number) => {
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * max);
+  } while (randomNumber === excludedNumber);
+  return randomNumber;
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[] | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [choosenPokemon, setChoosenPokemon] = useState<Pokemon | null>(null)
+  const [winner, setWinner] = useState<Pokemon | null>(null)
 
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchPokemon().then((result) => setPokemon(result))
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) return <CircularProgress />
+
+  if (!pokemon) return <Alert variant="outlined" severity="error">
+    Couldn't fetch pokemon. Try again later
+  </Alert>
+
+  let randomPokemon
+  if (choosenPokemon) randomPokemon = pokemon[getRandomExcluding(pokemon.indexOf(choosenPokemon), pokemon.length + 1)]
   return (
     <Container
       sx={{
@@ -66,94 +47,13 @@ function App() {
         gap: 3,
         py: 1.5
       }}>
-      <Typography variant="h3" >
-        Battle of Pokemon
-      </Typography>
+      <Navbar />
 
-      <Box>
-        <Typography variant="h5" mb={1}>
-          Select your pokemon
-        </Typography>
-        <Grid container justifyContent={{xs: "space-around", sm: "space-between"}} gap={{ xs: 2, sm: 1 }}>
-          {placeholderPokemon.map((pokemon) => <Grid
-            key={pokemon.id}
-            size={{ xs: 6, sm:2}}
-            sx={{
-              boxShadow: 12,
-              borderRadius: 4,
-              padding: 1,
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              maxWidth: "150px",
-            }}>
+      <PokemonGrid pokemon={pokemon} setChoosenPokemon={setChoosenPokemon} />
 
-            <img src={pokemon.imageUrl} alt={`Image of ${pokemon.name}`} style={{ width: "80%", maxWidth: "7rem", height: "auto", alignSelf: "center" }} />
-            <Typography pt={1}>
-              {pokemon.name}
-            </Typography>
-          </Grid>)}
-        </Grid>
-        {/* <Stack direction={{ xs: "column", sm: "row" }} gap={2} justifyContent={"space-between"} alignItems={"center"} >
-          {placeholderPokemon.map((pokemon) => <Box
-            key={pokemon.id}
-            sx={{
-              boxShadow: 12,
-              borderRadius: 4,
-              padding: 1,
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              maxWidth: "150px",
-            }}>
+      {winner && <WinnerBanner winner={winner} />}
 
-            <img src={pokemon.imageUrl} alt={`Image of ${pokemon.name}`} style={{ width: "80%", maxWidth: "7rem", height: "auto", alignSelf: "center" }} />
-            <Typography pt={1}>
-              {pokemon.name}
-            </Typography>
-          </Box>)}
-        </Stack> */}
-      </Box>
-
-      <Card sx={{
-        width: "100%",
-        border: "3px solid",
-        borderColor: "#377538",
-        p: 1,
-        boxShadow: 12
-      }}
-      >
-        <Typography variant="h5">Pikachu wins!</Typography>
-      </Card>
-
-      <Stack direction={"row"} sx={{
-        width: "100%",
-        gap: 2,
-        justifyContent: "space-between",
-      }}>
-        <PokemonCard pokemon={placeholderPokemon[0]} />
-        <Box sx={{
-          display: "flex",
-          alignItems: "center",
-        }} >
-          <Button variant="contained" sx={{
-            backgroundColor: "#377538",
-            textTransform: "none",
-            display: "none",
-            sm: { display: "flex" }
-          }}>
-            Start Battle
-          </Button>
-        </Box>
-        <PokemonCard pokemon={placeholderPokemon[4]} />
-      </Stack>
-      <Button variant="contained" sx={{
-        backgroundColor: "#377538",
-        textTransform: "none",
-        sm: { display: "none" }
-      }}>
-        Start Battle
-      </Button>
+      {choosenPokemon && <BattleSection choosenPokemon={choosenPokemon} setWinner={setWinner} randomPokemon={randomPokemon!} />}
     </Container>
   )
 }
