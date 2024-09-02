b332b640874ac931a1e180bef6d52a8fea7b58d0
BrunoPierca <pierca18@gmail.com>
2024-09-02 03:14:33 -0300

    added sqlite db

--- backend-challenge/src/app.module.ts
+++ backend-challenge/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration, JoiValidationSchema } from './config';
+import { BattleModule } from './battle/battle.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DB_FILE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    }),
+    PokemonModule,
+    BattleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
