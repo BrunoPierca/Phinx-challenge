ccedce69e43d560c30e50fbd0fca3ab008f361d4
BrunoPierca <pierca18@gmail.com>
2024-09-02 03:14:54 -0300

    Update pokemon.entity.ts

--- backend-challenge/src/pokemon/entities/pokemon.entity.ts
+++ backend-challenge/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,30 @@
-export class Pokemon {}
+import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn } from 'typeorm';
+
+
+@Entity()
+export class Pokemon {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    attack: number;
+    
+    @Column()
+    defense: number;
+
+    @Column()
+    hp: number;
+
+    @Column()
+    speed: number;
+
+
+    @Column()
+    type: string;
+
+    @Column()
+    imageUrl: string;
+}
